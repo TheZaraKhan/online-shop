c779287a537347e995f2575de9963283ad543d48
Zara Khan <ZaraMustafa@ymail.com>
2024-06-30 13:35:12 -0400

    deploy

--- user/backend/include/db.php
+++ user/backend/include/db.php
@@ -1,6 +1,8 @@
 <?php 
 
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: https://verenda.netlify.app");
+header("Access-Control-Allow-Credentials: true");
 
 // use mysqli to fetch database (host, username, password, dbname)
  $connection = new mysqli("localhost:8889", "root", "root", "myshop");
